61c9944d19670ce93ca4d9f3bf444beeddbee933
Lucas <Lucas@Lucas_Albertoni>
2014-04-21 09:41:14 -0300

    Criação do atributo.

--- Testandoo/src/br/com/teste/Pessoa.java
+++ Testandoo/src/br/com/teste/Pessoa.java
@@ -1,5 +1,14 @@
 package br.com.teste;
 
 public class Pessoa {
+	
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
 }
